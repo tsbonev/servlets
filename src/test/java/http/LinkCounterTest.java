299e47687fef8a9022efe4fd586a8e5d5645b89d
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-27 11:15:00 +0300

    Add multiple click test

--- src/test/java/http/LinkCounterTest.java
+++ src/test/java/http/LinkCounterTest.java
@@ -14,9 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
-
 public class LinkCounterTest {
 
     @Rule
@@ -57,6 +54,47 @@ public class LinkCounterTest {
         }});
 
         linkCounter.doGet(req, resp);
+
+    }
+
+    @Test
+    public void linkCounterRemembersCount() throws ServletException, IOException{
+
+        context.checking(new Expectations(){{
+
+            oneOf(req).getParameter("link"); will(returnValue("2"));
+            oneOf(req).setAttribute("linkOneCount", 0);
+            oneOf(req).setAttribute("linkTwoCount", 1);
+            oneOf(req).setAttribute("linkThreeCount", 0);
+            oneOf(req).getRequestDispatcher("counter.jsp"); will(returnValue(rd));
+            oneOf(rd).forward(req, resp);
+
+            oneOf(req).getParameter("link"); will(returnValue("3"));
+            oneOf(req).setAttribute("linkOneCount", 0);
+            oneOf(req).setAttribute("linkTwoCount", 1);
+            oneOf(req).setAttribute("linkThreeCount", 1);
+            oneOf(req).getRequestDispatcher("counter.jsp"); will(returnValue(rd));
+            oneOf(rd).forward(req, resp);
+
+            oneOf(req).getParameter("link"); will(returnValue("1"));
+            oneOf(req).setAttribute("linkOneCount", 1);
+            oneOf(req).setAttribute("linkTwoCount", 1);
+            oneOf(req).setAttribute("linkThreeCount", 1);
+            oneOf(req).getRequestDispatcher("counter.jsp"); will(returnValue(rd));
+            oneOf(rd).forward(req, resp);
+
+            oneOf(req).getParameter("link"); will(returnValue("1"));
+            oneOf(req).setAttribute("linkOneCount", 2);
+            oneOf(req).setAttribute("linkTwoCount", 1);
+            oneOf(req).setAttribute("linkThreeCount", 1);
+            oneOf(req).getRequestDispatcher("counter.jsp"); will(returnValue(rd));
+            oneOf(rd).forward(req, resp);
+
+        }});
+
+        for(int i = 0; i < 4; i++){
+            linkCounter.doGet(req, resp);
+        }
 
     }
 
